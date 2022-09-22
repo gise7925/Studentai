af0ec2ea0590ade2ad2c8ba60aecbcf0daa006da
seibokaite_G <113164706+gise7925@users.noreply.github.com>
2022-09-22 16:37:16 +0300

    Add files via upload
    Variantai atsižvelgiant į pastabas buvo pataisyti.

---
+++ mylib.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using std::cout;
+using std::cin;
+using std::string;
+using namespace std;
+using std::endl;
+using std::vector;
